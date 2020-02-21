a2ce49730912de30ef8064af32e8f2599ea13787
Ouestionista <55045420+RiddhiWhiteHat@users.noreply.github.com>
2020-02-21 10:37:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var drawing = [];
 var currentPath = [];
 
 function  setup() {
-  canvas = createCanvas(600,200);
+  canvas = createCanvas(600,600);
   canvas.mousePressed(startPath);
   var config = {
     apiKey: "AIzaSyD3Nvy2OOYEjeik3tPG1I7wqoA1WXIkyJo",
